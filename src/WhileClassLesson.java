4b092a1ce151d21849ce1573acd0576222242eb3
Andrey Sokolov <a.sokolov@hh.ru>
2018-04-26 14:12:10 +0300

    First Commit

---
+++ src/WhileClassLesson.java
@@ -0,0 +1,10 @@
+public class WhileClassLesson {
+    public static void main (String[] args) {
+        int x=1;
+        System.out.println("Какая-то шляпа");
+                while (x<20) {System.out.println("Внутри цикла");
+                    System.out.println("Значение x равно " +x);
+                x=x+1;}
+
+    }
+}
