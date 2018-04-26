17546f2c85df7e13ebc0eeb019e315f48a0fd884
Andrey Sokolov <a.sokolov@hh.ru>
2018-04-26 17:47:49 +0300

    IfClassLesson1

--- src/IfClassLesson.java
+++ src/IfClassLesson.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class IfClassLesson {
+    public static void main (String[] args) {
+        int x=1;
+        while (x<10) { x = x + 1;
+        if (x==3)
+            System.out.println("x должен равняться 3");
+        }
+        System.out.println("А это будет выполняться в любом случае");
+    }
 }
